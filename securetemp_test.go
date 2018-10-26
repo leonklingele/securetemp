dddef8dad67372dc0fb77e94177642ca78375b0c
Leon Klingele <git@leonklingele.de>
2018-10-26 18:12:58 +0200

    Improve test error messages

--- securetemp_test.go
+++ securetemp_test.go
@@ -12,29 +12,29 @@ const (
 func TestTempDir(t *testing.T) {
 	tmpDir, cleanupFunc, err := TempDir(size)
 	if err != nil {
-		t.Fatal("failed to create temp dir", err)
+		t.Fatalf("failed to create temp dir: %v", err)
 	}
 	if _, err := os.Stat(tmpDir); err != nil {
-		t.Fatal("something is wrong with the temp dir", err)
+		t.Fatalf("something is wrong with the temp dir: %v", err)
 	}
 
 	cleanupFunc()
 	if _, err := os.Stat(tmpDir); err == nil {
-		t.Fatal("temp dir should no longer exist after cleanup, but does", tmpDir)
+		t.Fatalf("temp dir %q should no longer exist after cleanup, but does", tmpDir)
 	}
 }
 
 func TestTempFile(t *testing.T) {
 	tmpFile, cleanupFunc, err := TempFile(size)
 	if err != nil {
-		t.Fatal("failed to create temp file", err)
+		t.Fatalf("failed to create temp file: %v", err)
 	}
 	if _, err := tmpFile.Stat(); err != nil {
-		t.Fatal("something is wrong with the temp file", err)
+		t.Fatalf("something is wrong with the temp file: %v", err)
 	}
 
 	cleanupFunc()
 	if _, err := tmpFile.Stat(); err == nil {
-		t.Fatal("temp dir should no longer exist after cleanup, but does", tmpFile)
+		t.Fatalf("temp dir %q should no longer exist after cleanup, but does", tmpFile.Name())
 	}
 }
